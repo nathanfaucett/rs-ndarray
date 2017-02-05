e229ec5584e629c150f8452cb1c3f72b4b853632
nathanfaucett <nathanfaucett@gmail.com>
2017-02-05 15:10:11 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+rs-ndarray [![Build Status](https://travis-ci.org/nathanfaucett/rs-ndarray.svg?branch=master)](https://travis-ci.org/nathanfaucett/rs-ndarray)
+=====
+N-dimensional array
---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+extern crate array;
+extern crate min;
+extern crate one;
+extern crate zero;
+
+
+//mod ndarray_builder;
+mod darray;
+
+
+//pub use ndarray_builder::NDArrayBuilder;
+pub use darray::DArray;
